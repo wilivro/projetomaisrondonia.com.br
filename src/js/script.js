515e70eaea9de60d15a4ac89bfdf3eddd321ec67
Saulo Nogueira <sauloln@gmail.com>
2023-04-28 19:25:36 -0300

    ajustando botao

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,3 @@
-var organization_token = '';
 var icon_color = 'null';
 var init_minimal = 'true';
 var show_chat_icon = 'true';
@@ -8,14 +7,4 @@ $("#chat").click(function () {
   $(".chat__clearfix").trigger('click');
 });
 
-const testinterval = () => {
-  const safe = setInterval(() => {
-  if($(".chat__clearfix").length) {
-    setTimeout(() => {
-      $(".chat__clearfix").trigger('click');
-    },100)
-    clearInterval(safe)
-  }
-  }, 100); 
-}
-testinterval()
+
